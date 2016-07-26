6cc20fabdc241e019889cbe99dc13ee85910cc2d
Suvankar Mitra <suvankarmitra.me@gmail.com>
2016-07-26 16:14:16 +0530

    static expression done

--- src/maths/Driver.java
+++ src/maths/Driver.java
@@ -3,7 +3,7 @@ package maths;
 //this is a demo class
 public class Driver {
 	public static void main(String[] args) {
-		String expr = "5! + sin(30) - ln(10) + 2 * pi / e";	//demo expression
+		String expr = "tan( 0- 2)";	//demo expression
 		
 		Double d = ExpressionEvaluator.evaluate(expr);		//evaluating expression
 		
