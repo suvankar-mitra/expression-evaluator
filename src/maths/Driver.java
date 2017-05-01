3a7ad1a99259ea80b73565c687c4bf9d7b84d975
Sameer Sah <sameer_sah@yahoo.co.in>
2017-05-01 20:13:07 +0530

    Take input from user (console)

--- src/maths/Driver.java
+++ src/maths/Driver.java
@@ -1,9 +1,15 @@
 package maths;
 
+import java.util.Scanner;
+
 //this is a demo class
 public class Driver {
 	public static void main(String[] args) {
-		String expr = "tan( 0- 2)";	//demo expression
+	//	String expr = "tan( 0- 2)";	//demo expression
+		
+		//Take input from console(user)
+		Scanner sc = new Scanner(System.in);
+	    String expr =sc.next();
 		
 		Double d = ExpressionEvaluator.evaluate(expr);		//evaluating expression
 		
